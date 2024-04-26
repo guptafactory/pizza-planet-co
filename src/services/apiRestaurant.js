1fe69290d5e56f9472aa5708ba79b74e53b40c14
Himanshu Gupta <guptafactory@gmail.com>
2024-04-26 19:28:06 +0530

    Added Order Logic

---
+++ src/services/apiRestaurant.js
@@ -0,0 +1,44 @@
+const API_URL = "https://react-fast-pizza-api.onrender.com/api";
+
+export async function getMenu() {
+  const res = await fetch(`${API_URL}/menu`);
+  if (!res.ok) throw Error("Failed getting menu");
+
+  const { data } = await res.json();
+  return data;
+}
+
+export async function getOrder(id) {
+  const res = await fetch(`${API_URL}/order/${id}`);
+  if (!res.ok) throw Error(`Couldn't find order #${id}`);
+
+  const { data } = await res.json();
+  return data;
+}
+
+export async function createOrder(newOrder) {
+  const res = await fetch(`${API_URL}/order`, {
+    method: "POST",
+    body: JSON.stringify(newOrder),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (!res.ok) throw Error("Failed creating your order");
+  const { data } = await res.json();
+  return data;
+}
+
+export async function updateOrder(id, updateObj) {
+  const res = await fetch(`${API_URL}/order/${id}`, {
+    method: "PATCH",
+    body: JSON.stringify(updateObj),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (!res.ok) throw Error("Failed updating your order");
+  // We don't need the data, so we don't return anything
+}
---
+++ src/ui/AppLayout.jsx
@@ -0,0 +1,23 @@
+import { Outlet, useNavigation } from "react-router-dom";
+
+import Header from "./Header";
+import CartOverview from "./../features/cart/CartOverview";
+import PageLoader from "./PageLoader";
+
+function AppLayout() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading"; // "laoding" or "idle"
+
+  return (
+    <div className="layout">
+      {isLoading && <PageLoader />}
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+      <CartOverview />
+    </div>
+  );
+}
+
+export default AppLayout;
