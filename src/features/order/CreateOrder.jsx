4059c0f778391be32030d8a2e46b2137115a553e
Himanshu Gupta <guptafactory@gmail.com>
2024-04-27 19:05:55 +0530

    Added: Tailwind

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -5,7 +5,7 @@ import { createOrder } from "../../services/apiRestaurant";
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
-    str
+    str,
   );
 
 const fakeCart = [
@@ -78,7 +78,10 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <button disabled={isSubmitting}>
+          <button
+            disabled={isSubmitting}
+            className="focus: focus: inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 outline-none ring-yellow-300 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:ring focus:ring-offset-2 disabled:cursor-not-allowed"
+          >
             {isSubmitting ? "Placing Order 🚀" : "Order now"}
           </button>
         </div>
@@ -103,8 +106,9 @@ export async function action({ request }) {
 
   if (Object.keys(errors).length > 0) return errors;
 
-  const newOrder = await createOrder(order);
+  // const newOrder = await createOrder(order);
 
-  return redirect(`/order/${newOrder.id}`);
+  // return redirect(`/order/${newOrder.id}`);
+  return null;
 }
 export default CreateOrder;
---
+++ src/features/user/Username.jsx
@@ -0,0 +1,9 @@
+function Username() {
+  return (
+    <div className="hidden text-sm md:block">
+      <p className="font-semibold ">Himanshu</p>
+    </div>
+  );
+}
+
+export default Username;
